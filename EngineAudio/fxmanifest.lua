9901c1bdf909e6aad4a3e577a8b4f59f350371db
Jake Murray <j1993murray@hotmail.com>
2024-11-23 22:07:31 +0000

    Upload: EngineAudio folders

---
+++ EngineAudio/fxmanifest.lua
@@ -0,0 +1,182 @@
+fx_version "cerulean"
+game "gta5"
+
+files {
+    "audioconfig/*.rel",
+    "sfx/**/*.awc",
+}
+
+
+-- ALPHAV
+data_file "AUDIO_GAMEDATA" "audioconfig/alphav_game.dat"
+data_file "AUDIO_SOUNDDATA" "audioconfig/alphav_sounds.dat"
+data_file "AUDIO_WAVEPACK" "sfx/dlc_alphav"
+
+-- ROXANNE
+data_file "AUDIO_GAMEDATA" "audioconfig/roxanne_game.dat"
+data_file "AUDIO_SOUNDDATA" "audioconfig/roxanne_sounds.dat"
+data_file "AUDIO_WAVEPACK" "sfx/dlc_roxanne"
+
+-- SEVERO
+data_file "AUDIO_GAMEDATA" "audioconfig/severo_game.dat"
+data_file "AUDIO_SOUNDDATA" "audioconfig/severo_sounds.dat"
+data_file "AUDIO_WAVEPACK" "sfx/dlc_severo"
+
+-- STRATUMC
+data_file "AUDIO_SYNTHDATA" "audioconfig/stratumc_amp.dat"
+data_file "AUDIO_GAMEDATA" "audioconfig/stratumc_game.dat"
+data_file "AUDIO_SOUNDDATA" "audioconfig/stratumc_sounds.dat"
+data_file "AUDIO_WAVEPACK" "sfx/dlc_zircoflow"
+
+-- SULTANRSV8
+data_file "AUDIO_GAMEDATA" "audioconfig/sultanrsv8_game.dat"
+data_file "AUDIO_SOUNDDATA" "audioconfig/sultanrsv8_sounds.dat"
+data_file "AUDIO_WAVEPACK" "sfx/dlc_v8sultanrs"
+
+-- TURISMOC
+data_file "AUDIO_GAMEDATA" "audioconfig/turismoc_game.dat"
+data_file "AUDIO_SOUNDDATA" "audioconfig/turismoc_sounds.dat"
+data_file "AUDIO_WAVEPACK" "sfx/dlc_turismoc"
+
+-- PATRIOTS
+data_file "AUDIO_GAMEDATA" "audioconfig/patriots_game.dat"
+data_file "AUDIO_SOUNDDATA" "audioconfig/patriots_sounds.dat"
+data_file "AUDIO_WAVEPACK" "sfx/dlc_patriots"
+
+-- TAMPAX2
+data_file "AUDIO_GAMEDATA" "audioconfig/tampar_game.dat"
+data_file "AUDIO_SOUNDDATA" "audioconfig/tampar_sounds.dat"
+data_file "AUDIO_WAVEPACK" "sfx/dlc_tampar"
+
+-- ELEGYRH7
+data_file "AUDIO_GAMEDATA" "audioconfig/elegyrh7_game.dat"
+data_file "AUDIO_SOUNDDATA" "audioconfig/elegyrh7_sounds.dat"
+data_file "AUDIO_WAVEPACK" "sfx/dlc_rh7elegy"
+
+-- ELEGYRH5
+data_file "AUDIO_GAMEDATA" "audioconfig/elegyx_game.dat"
+data_file "AUDIO_SOUNDDATA" "audioconfig/elegyx_sounds.dat"
+data_file "AUDIO_WAVEPACK" "sfx/dlc_elegyx"
+
+-- ELEGYRH4
+data_file "AUDIO_GAMEDATA" "audioconfig/rh4_game.dat"
+data_file "AUDIO_SOUNDDATA" "audioconfig/rh4_sounds.dat"
+data_file "AUDIO_WAVEPACK" "sfx/dlc_rh4elegy"
+
+-- BLISTA2GP
+data_file "AUDIO_SYNTHDATA" "audioconfig/blisgpr_amp.dat"
+data_file "AUDIO_GAMEDATA"  "audioconfig/blisgpr_game.dat"
+data_file "AUDIO_SOUNDDATA" "audioconfig/blisgpr_sounds.dat"
+data_file "AUDIO_WAVEPACK"  "sfx/dlc_blisgpr"
+
+-- URBAN
+data_file "AUDIO_GAMEDATA" "audioconfig/urban_game.dat"
+data_file "AUDIO_SOUNDDATA" "audioconfig/urban_sounds.dat"
+data_file "AUDIO_WAVEPACK" "sfx/dlc_urban"
+
+-- 2024 Mustang GT
+data_file "AUDIO_GAMEDATA" "audioconfig/cammedcharger_game.dat"
+data_file "AUDIO_SOUNDDATA" "audioconfig/cammedcharger_sounds.dat"
+data_file "AUDIO_WAVEPACK" "sfx/dlc_cammedcharger"
+
+-- vulcanpro19
+data_file "AUDIO_GAMEDATA" "audioconfig/aston59v12_game.dat"
+data_file "AUDIO_SOUNDDATA" "audioconfig/aston59v12_sounds.dat"
+data_file "AUDIO_WAVEPACK" "sfx/dlc_aston59v12"
+
+-- awela (Eleanor, mustangv8 extremely loud)
+data_file "AUDIO_GAMEDATA" "audioconfig/lg57mustangtv8_game.dat"
+data_file "AUDIO_SOUNDDATA" "audioconfig/lg57mustangtv8_sounds.dat"
+data_file "AUDIO_WAVEPACK" "sfx/dlc_lg57mustangtv8"
+
+-- AMG Black Series/SchlagenSTR
+data_file "AUDIO_GAMEDATA" "audioconfig/ta178amgb_game.dat"
+data_file "AUDIO_SOUNDDATA" "audioconfig/ta178amgb_sounds.dat"
+data_file "AUDIO_WAVEPACK" "sfx/dlc_ta178amgb"
+
+-- stewarias (arias)
+data_file "AUDIO_GAMEDATA" "audioconfig/arias_game.dat"
+data_file "AUDIO_SOUNDDATA" "audioconfig/arias_sounds.dat"
+data_file "AUDIO_WAVEPACK" "sfx/dlc_stewarias"
+
+-- Ford GT MKII 2017 (not in use)
+data_file "AUDIO_GAMEDATA" "audioconfig/aq46forgtebv6_game.dat"
+data_file "AUDIO_SOUNDDATA" "audioconfig/aq46forgtebv6_sounds.dat"
+data_file "AUDIO_WAVEPACK" "sfx/dlc_aq46forgtebv6"
+         
+-- Corvette C7 (not in use)
+data_file "AUDIO_GAMEDATA" "audioconfig/chevroletlt4_game.dat"
+data_file "AUDIO_SOUNDDATA" "audioconfig/chevroletlt4_sounds.dat"
+data_file "AUDIO_WAVEPACK" "sfx/dlc_chevroletlt4"
+
+-- V8 69 Mustang (not in use)
+data_file "AUDIO_GAMEDATA" "audioconfig/ta008mustang69_game.dat"
+data_file "AUDIO_SOUNDDATA" "audioconfig/ta008mustang69_sounds.dat"
+data_file "AUDIO_WAVEPACK" "sfx/dlc_ta008mustang69"
+
+-- zinxy zr350 
+data_file "AUDIO_GAMEDATA" "audioconfig/aq31maz13btune_game.dat"
+data_file "AUDIO_SOUNDDATA" "audioconfig/aq31maz13btune_sounds.dat"
+data_file "AUDIO_WAVEPACK" "sfx/dlc_aq31maz13btune"
+
+-- flivver
+data_file "AUDIO_GAMEDATA" "audioconfig/sfxflivver_game.dat"
+data_file "AUDIO_SOUNDDATA" "audioconfig/sfxflivver_sounds.dat"
+data_file "AUDIO_WAVEPACK" "sfx/dlc_flivver"
+
+-- spritzer
+data_file "AUDIO_GAMEDATA" "audioconfig/spritzer_game.dat"
+data_file "AUDIO_SOUNDDATA" "audioconfig/spritzer_sounds.dat"
+data_file "AUDIO_WAVEPACK" "sfx/dlc_spritzer"
+
+-- callista
+data_file "AUDIO_GAMEDATA" "audioconfig/callista_game.dat"
+data_file "AUDIO_SOUNDDATA" "audioconfig/callista_sounds.dat"
+data_file "AUDIO_WAVEPACK" "sfx/dlc_callista"
+
+-- coquette tuned up sound (not in use)
+data_file "AUDIO_GAMEDATA" "audioconfig/coquette4c_game.dat"
+data_file "AUDIO_SOUNDDATA" "audioconfig/coquette4c_sounds.dat"
+data_file "AUDIO_WAVEPACK" "sfx/dlc_coquette4c"
+
+-- meteor
+data_file "AUDIO_SYNTHDATA" "audioconfig/meteor_amp.dat"
+data_file "AUDIO_GAMEDATA"  "audioconfig/meteor_game.dat"
+data_file "AUDIO_SOUNDDATA" "audioconfig/meteor_sounds.dat"
+data_file "AUDIO_WAVEPACK"  "sfx/dlc_meteor"
+
+-- turismo2lm
+data_file "AUDIO_GAMEDATA" "audioconfig/turismo2lm_game.dat"
+data_file "AUDIO_SOUNDDATA" "audioconfig/turismo2lm_sounds.dat"
+data_file "AUDIO_WAVEPACK" "sfx/dlc_turismo2lm"
+
+-- sunrise
+data_file "AUDIO_GAMEDATA" "audioconfig/sunrise_game.dat"
+data_file "AUDIO_SOUNDDATA" "audioconfig/sunrise_sounds.dat"
+data_file "AUDIO_WAVEPACK" "sfx/dlc_sunrise"
+
+-- rx7bpeng as_zr350 
+data_file "AUDIO_SYNTHDATA" "audioconfig/rx7bpeng_amp.dat"
+data_file "AUDIO_GAMEDATA"  "audioconfig/rx7bpeng_game.dat"
+data_file "AUDIO_SOUNDDATA" "audioconfig/rx7bpeng_sounds.dat"
+data_file "AUDIO_WAVEPACK"  "sfx/dlc_rx7bpeng"
+
+-- jester5
+data_file "AUDIO_GAMEDATA" "audioconfig/aqtoy2jzstock_game.dat"
+data_file "AUDIO_SOUNDDATA" "audioconfig/aqtoy2jzstock_sounds.dat"
+data_file "AUDIO_WAVEPACK" "sfx/dlc_aqtoy2jzstock"
+
+-- charchap
+data_file "AUDIO_GAMEDATA" "audioconfig/aq2jzgterace_game.dat"
+data_file "AUDIO_SOUNDDATA" "audioconfig/aq2jzgterace_sounds.dat"
+data_file "AUDIO_WAVEPACK" "sfx/dlc_aq2jzgterace"
+
+-- remustwo
+data_file "AUDIO_GAMEDATA" "audioconfig/remustwo_game.dat"
+data_file "AUDIO_SOUNDDATA" "audioconfig/remustwo_sounds.dat"
+data_file "AUDIO_WAVEPACK" "sfx/dlc_remustwo"
+
+-- furiawb
+data_file "AUDIO_GAMEDATA" "audioconfig/furiawb_game.dat"
+data_file "AUDIO_SOUNDDATA" "audioconfig/furiawb_sounds.dat"
+data_file "AUDIO_WAVEPACK" "sfx/dlc_wbfuria"
