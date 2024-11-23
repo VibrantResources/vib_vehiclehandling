0c3c173cc690963f24a6951ff4787892fd2cac3e
Vibrant <127861623+VibrantRP@users.noreply.github.com>
2024-11-23 22:00:25 +0000

    Upload: vehiclehandling folder
    - carcols
    - carvariations
    - fxmanifest
    - handling.meta
    - vehicles.meta

---
+++ vehiclehandling/fxmanifest.lua
@@ -0,0 +1,12 @@
+fx_version 'cerulean'
+game 'gta5'
+
+files { '*.meta',
+        'handling/*.meta',
+    }
+
+data_file 'HANDLING_FILE'          'handling/*handling.meta'
+data_file 'VEHICLE_METADATA_FILE'  '*vehicles.meta'
+data_file 'CARCOLS_FILE'           '*carcols.meta'
+data_file 'VEHICLE_VARIATION_FILE' '*carvariations.meta'
+data_file 'VEHICLE_LAYOUTS_FILE'   '*vehiclelayouts.meta'
